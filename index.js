cf3213befee8a3e483dc41b1500132dfbb1af878
rjspyk5 <rjspyk5@gmail.com>
2024-02-22 18:42:48 +0600

    total price and grand total functionality added

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 const titleContainer = document.getElementById("title-container");
 let serial = 1;
+const productOnCart = {};
+let totalPrice = 0;
 
 // /Add button on every card
 const cards = document.getElementsByClassName("card");
 for (const card of cards) {
   const button = document.createElement("button");
-
   button.setAttribute("type", "button");
   button.innerText = "Add to cart";
   button.className = "btn btn-secondary addcart";
   card.appendChild(button);
 }
 
+// Push products details on a object
+
 // Appending product name into the Product container
 const appendProductIntoCart = (productName) => {
-  //  Create new p tag for append into title-container
   const p = document.createElement("p");
   p.innerText = `${serial}. ${productName}`;
   serial++;
-  //append prodcut name into title-container
   titleContainer.appendChild(p);
-  //   Quantity-heading display shown
   const quantityHeading = document.getElementById("quantity-heading");
   quantityHeading.classList.remove("hidden");
 };
 
 // Cheaking Quantity
 const cheakingQuantity = () => {};
-
 // Fuction about when someone click on add to cart
 const handleAddCart = (e) => {
   const productName = e.target.previousElementSibling.children[1].innerText;
@@ -35,6 +34,18 @@ const handleAddCart = (e) => {
     e.target.previousElementSibling.children[2].innerText.split(" ")[1]
   );
   appendProductIntoCart(productName);
+  totalPrice += productPrice;
+  setTotalPrice(totalPrice);
+  setGrandTotal(totalPrice);
+};
+// Set Total Price
+const setTotalPrice = (totalPrice) => {
+  document.getElementById("totalPrice").innerText = totalPrice;
+};
+// set grand total
+const setGrandTotal = (totalPrice, copun = 0) => {
+  document.getElementById("total").innerText =
+    totalPrice - totalPrice * (copun * 0.2);
 };
 
 // Add event listener to every add to cart button
